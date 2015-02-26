6dc4d000af7b2b794ac0093c676a4e22e1086a00
Mason Weber <mason.weber.cpe@gmail.com>
2015-02-26 15:49:36 -0700

    recommit 1

---
+++ JUnit-Sample/src/sample/Case.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sample;
+
+/**
+ *
+ * @author Mason
+ */
+public class Case {
+    public Case() {
+        
+    }
+    
+    private int add(int a, int b) {
+        return a + b;
+    }
+    
+    private int sub(int a, int b) {
+        return a - b;
+    }
+    
+    private int mult(int a, int b) {
+        return a * b;
+    }
+    
+    private int div(int a, int b) {
+        return a / b;
+    }
+    
+    private int mod(int a, int b) {
+        return a % b;
+    }
+}
---
+++ JUnit-Sample/test/sample/CaseTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mason
+ */
+public class CaseTest {
+    
+    public CaseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAdd() {
+        // TODO review the generated test code and remove the default call to fail.
+        //assertTrue(case.add)
+    }
+    
+    @Test
+    public void testSub() {
+        
+    }
+    
+    @Test
+    public void testMult() {
+        
+    }
+    
+    @Test
+    public void testDiv() {
+        
+    }
+    
+    @Test
+    public void testMod() {
+        
+    }
+}
